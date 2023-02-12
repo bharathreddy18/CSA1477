b7b75a5f4ee0b1dbc737077a29cfd27dbdcc5d61
bharathreddy18 <124851785+bharathreddy18@users.noreply.github.com>
2023-02-12 16:22:41 +0530

    Update FOLLOW.c

--- FOLLOW.c
+++ FOLLOW.c
@@ -1,105 +1,67 @@
-#include<stdio.h> 
-#include<ctype.h>
+#include<stdio.h>
 #include<string.h>
-int limit, x = 0;
-char production[10][10], array[10];
- 
-void find_first(char ch);
-void find_follow(char ch);
-void Array_Manipulation(char ch);
- 
+int n,m=0,p,i=0,j=0;
+char a[10][10],f[10];
+void follow(char c);
+void first(char c);
 int main()
 {
-      int count;
-      char option, ch;
-      printf("\nEnter Total Number of Productions:\t");
-      scanf("%d", &limit);
-      for(count = 0; count < limit; count++)
-      {
-            printf("\nValue of Production Number [%d]:\t", count + 1);
-            scanf("%s", production[count]);
-      }
-      do
-      {
-            x = 0;
-            printf("\nEnter production Value to Find Follow:\t");
-            scanf(" %c", &ch);
-            find_follow(ch);
-            printf("\nFollow Value of %c:\t{ ", ch);
-            for(count = 0; count < x; count++)
-            {
-                  printf("%c ", array[count]);
-            }
-            printf("}\n");
-            printf("To Continue, Press Y:\t");
-            scanf(" %c", &option);
-      }while(option == 'y' || option == 'Y');
-      return 0;
+ int i,choice;
+ char c,ch;
+ printf("Enter the no.of productions:");
+ scanf("%d",&n);
+ printf("Enter the productions(epsilon=@):\n");
+ for(i=0;i<n;i++)
+  scanf("%s%c",a[i],&ch);
+do
+ {
+  m=0;
+  printf("Enter the element whose FOLLOW is to be found:");
+scanf("%c",&c);
+  follow(c);
+  printf("FOLLOW(%c) = { ",c);
+  for(i=0;i<m;i++)
+   printf("%c ",f[i]);
+  printf(" }\n");
+  printf("Do you want to continue(0/1)?");
+  scanf("%d%c",&choice,&ch);
+ }
+ while(choice==1);
 }
- 
-void find_follow(char ch)
+void follow(char c)
 {
-      int i, j;
-      int length = strlen(production[i]);
-      if(production[0][0] == ch)
-      {
-            Array_Manipulation('$');
-      }
-      for(i = 0; i < limit; i++)
-      {
-            for(j = 2; j < length; j++)
-            {
-                  if(production[i][j] == ch)
-                  {
-                        if(production[i][j + 1] != '\0')
-                        {
-                              find_first(production[i][j + 1]);
-                        }
-                        if(production[i][j + 1] == '\0' && ch != production[i][0])
-                        {     
-                              find_follow(production[i][0]);
-                        }
-                  }
-            }
-      }
+
+ if(a[0][0]==c)f[m++]='$';
+ for(i=0;i<n;i++)
+ {
+  for(j=2;j<strlen(a[i]);j++)
+  {
+   if(a[i][j]==c)
+   {
+    if(a[i][j+1]!='\0')first(a[i][j+1]);
+
+    if(a[i][j+1]=='\0'&&c!=a[i][0])
+     follow(a[i][0]);
+
+   }
+  }
+ }
 }
- 
-void find_first(char ch)
+void first(char c)
 {
-      int i, k;
-      if(!(isupper(ch)))
-      {
-            Array_Manipulation(ch);
-      }
-      for(k = 0; k < limit; k++)
-      {
-            if(production[k][0] == ch)
-            {
-                  if(production[k][2] == '$') 
-                  {
-                        find_follow(production[i][0]);
-                  }
-                  else if(islower(production[k][2]))
-                  {
-                        Array_Manipulation(production[k][2]);
-                  }
-                  else 
-                  {
-                        find_first(production[k][2]);
-                  }
-            }
-      }
-}
- 
-void Array_Manipulation(char ch)
-{
-      int count;
-      for(count = 0; count <= x; count++)
-      {
-            if(array[count] == ch)
-            {
-                  return;
-            }
-      }
-      array[x++] = ch;
+      int k;
+                 if(!(isupper(c)))f[m++]=c;
+                 for(k=0;k<n;k++)
+                 {
+                 if(a[k][0]==c)
+                 {
+                 if(a[k][2]=='@') 
+				 follow(a[i][0]);
+                 else if(islower(a[k][2]))
+				 f[m++]=a[k][2];
+                 else 
+				 first(a[k][2]);
+                 }
+                 }
+
 }
